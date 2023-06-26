55d414352e740a5ecacf313732074d9b4cf2a6b3
maralorn <mail@maralorn.de>
2023-06-26 23:21:50 +0200

    Remove obsolete Alternative instance
    Upstream now implements an Alternative instance, so we don’t need the Orphan anymore

--- src/Control/Monad/Logger/Orphans.hs
+++ src/Control/Monad/Logger/Orphans.hs
@@ -8,10 +8,5 @@ import Control.Monad.Logger
 
 import Control.Applicative (Alternative(..))
 import Control.Monad (MonadPlus(..))
-import Control.Monad.Trans (lift)
-
-instance (Monad m, Alternative m) => Alternative (LoggingT m) where
-  empty = lift empty
-  a <|> b = LoggingT $ \f -> runLoggingT a f <|> runLoggingT b f
 
 instance (Monad m, Alternative m) => MonadPlus (LoggingT m)
